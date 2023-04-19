58f83e8751001eb65bff132ff561ace1944f3468
Idan Lev <idan.93lev@gmail.com>
2023-04-19 09:57:00 +0300

    export Home at app/index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
     </View>
-  )
-}
+  );
+};
+
+export default Home;
