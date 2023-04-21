bd5d254a5f9b848c2ba2eeb26e64031a20ad9f4d
Idan Lev <idan.93lev@gmail.com>
2023-04-21 10:38:47 +0300

    added multiple components imports to app/index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router"
 
-import { COLORS, icons, images, SIZES } from "../constants"
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
 
 const Home = () => {
   return (
