7700d9076ce91fcc19bbb40aa8c1e765c600b36c
Idan Lev <idan.93lev@gmail.com>
2023-04-21 10:36:54 +0300

    added constants imports to app/index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router"
 
+import { COLORS, icons, images, SIZES } from "../constants"
+
 const Home = () => {
   return (
     <View>
