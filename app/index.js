551fa9cd46a63d1025f43a0009bd823e17d6d5d9
Idan Lev <idan.93lev@gmail.com>
2023-04-22 16:32:21 +0300

    added options to Stack jsx element at app/index.js

--- app/index.js
+++ app/index.js
@@ -14,8 +14,20 @@ const Home = () => {
   const router = useRouter();
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
-      <Stack.Screen />
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: ""
+        }}
+      />
     </SafeAreaView>
   );
 };
