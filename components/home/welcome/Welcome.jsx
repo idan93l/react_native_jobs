30ad35b908b7f88c304fbe5334d484fa3c0a0746
Idan Lev <idan.93lev@gmail.com>
2023-04-25 21:54:23 +0300

    added native imports to Welcome.jsx

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
 
 import styles from "./welcome.style";
 
