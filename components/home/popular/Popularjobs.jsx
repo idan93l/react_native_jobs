525716c7bbd92edc4401cadaf6efa9845e045c84
Idan Lev <idan.93lev@gmail.com>
2023-05-03 21:23:11 +0300

    added constants imports Popularjobs.jsx

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -3,6 +3,7 @@ import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
 
 const Popularjobs = () => {
   return (
