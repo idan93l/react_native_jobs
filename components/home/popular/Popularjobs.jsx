66a02fdb647691f1a106d76549d2a1affcb0ff21
Idan Lev <idan.93lev@gmail.com>
2023-05-04 21:25:15 +0300

    added PopularJobCard import at Popularjobs.jsx

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
   return (
