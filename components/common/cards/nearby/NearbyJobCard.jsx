4fde3b4adfeb2c13b9d57f3843f3dcb77ccfb498
Idan Lev <idan.93lev@gmail.com>
2023-05-09 12:17:35 +0300

    initialized JobDetails component

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,7 +1,6 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./nearbyjobcard.style";
-
 import { checkImageURL } from "../../../../utils";
 
 const NearbyJobCard = ({ job, handleNavigate }) => {
@@ -14,16 +13,17 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
               ? job.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
-          resizeMode="contain"
+          resizeMode='contain'
           style={styles.logoImage}
         />
       </TouchableOpacity>
-      
+
       <View style={styles.textContainer}>
         <Text style={styles.jobName} numberOfLines={1}>
-          {job.job_title}
+          {job?.job_title}
         </Text>
-        <Text style={styles.location}>{job.job_employment_type}</Text>
+
+        <Text style={styles.jobType}>{job?.job_employment_type}</Text>
       </View>
     </TouchableOpacity>
   );
